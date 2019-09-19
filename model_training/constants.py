6237e73c2f9b54cbdb1226391f2a11dc5e89f37f
hejp009 <hejunpeng2012@hotmail.com>
2019-09-19 17:33:59 +0800

    added more annotations

--- model_training/constants.py
+++ model_training/constants.py
@@ -15,33 +15,36 @@ LABELS = [pl, lf, cp, cs, pt, ds, dv, we, os, ap, pf]
 pl_list = ['JavaScript', 'Javascript', 'JS', 'TypeScript', 'CoffeeScript', 'HTML', 'CSS', 'CSS3', 'PHP', 'ES6', 'Java',
            'java', 'Kotlin', 'kotlin', 'Go', 'go', 'bash', 'C++', 'C', 'c++', 'C#', 'c#', 'Scala', 'ES2015', 'SCSS'
            'Python', 'python', 'Ruby', 'HTML5', 'ES8', 'Golang', 'Rust', "rust", 'Objective C', 'objective-c', 'Obj-C',
-           'swift',
+           'swift', 'Perl', 'Erlang', 'Jython',
            'Elixir', 'F#', 'ES7']
 lf_list = ['React JS', 'React.js', 'react.js', 'React', 'Redux', 'EcmaScript', 'Immutable.js', 'Babel', 'Webpack', 'NPM',
            'NGINX', 'Laravel', 'Flask', 'django', 'scikit-learn', 'Ruby on Rails', 'Rails', 'Node.js', 'NodeJS', 'Node',
-           'jQuery', 'JQuery', 'Koa', 'Drupal', 'Drush', "Behat"
+           'jQuery', 'JQuery', 'Koa', 'Drupal', 'Drush', "Behat", 'Symfony2 ', 'Matplotlib', 'OpenCV', 'Matplotlib,'
+           'Numpy', 'SciPy', 'pytest', 'Pytorch',
            'Jenkins', 'Backbone', 'backbone.js', 'jUnit', 'Mockito', 'Hamcrest', 'Dagger', 'Terraform', 'Ansible',
            'React Native', 'AngularJS', 'Angular', 'Jest', 'Karma', 'Protractor', 'Cypress', 'Vue.js', 'VUE', 'ASP.NET',
            'Apollo Client', 'WordPress', 'Elasticsearch', 'Selenium', 'machine learning', 'LAMP', 'MEAN', 'Next.js',
            'Scrapy', 'Celery', 'RabbitMQ', 'Express', 'RESTify', 'Bootstrap', 'RiotJS', 'LESS', 'Spring', 'Spring boot',
-           'Gradle', 'Maven', 'Magento', 'Odoo', 'Oboe']
+           'Gradle', 'Maven', 'Magento', 'Odoo', 'Oboe', 'Sqoop', 'Hadoop', 'Spark', 'Hive', 'YARN']
 
 cp_list = ['DOM', 'JavaScript object model', 'routing', 'switching']
 
 cs_list = ['data structure', 'algorithms', 'concurrency', 'object-oriented programming', 'design patterns',
-           'Computer Science', "distributed systems", 'OOP', 'Cryptography']
+           'Computer Science', "distributed systems", 'OOP', 'Cryptography', 'computability', 'computer architecture',
+           'concurrency', 'parallelism']
 
 pt_list = ['RESTful APIs', 'REST API', 'TCP/IP', 'UDP', 'browser APIs', 'HTTP', 'WebSockets', 'WebRTC', 'GraphQL', 'API',
            'firewalls', 'Serverless', 'CORS', 'SSL', 'Paxos', 'WASAPI', 'CoreAudio', 'wasm']
 
 ds_list = ['JSON', 'PostgreSQL', 'Postgres', 'MongoDB', 'mongodb', 'MySQL', 'mysql', 'ClustrixDB', 'Redis', 'redis', 'SQL',
-           'MariaDB', 'InnoDB', 'Kafka', 'Nomad, NoSQL', 'Dynamo', 'Phoenix', 'MS SQL', 'No SQL', 'Firebase']
+           'MariaDB', 'InnoDB', 'Kafka', 'Nomad, NoSQL', 'Dynamo', 'Phoenix', 'MS SQL', 'No SQL', 'Firebase', 'HBase']
 
 dv_list = ['front-end', 'frontend engineers', 'Web Architect', 'architect', 'Full Stack', 'full stack', 'Android Developer',
            'Android engineer', 'Game Developer', 'artificial intelligence', '3D Rendering',
            'UI designs', 'UI', 'UX', 'AI', 'Rails Engineer', 'Product Managers', 'Designers', 'CTO', 'Web Developer',
            'Full Stack Developer', 'QA testers', 'DevOps', 'frontend stack', 'frontend UIs', 'product designers',
-           'Product Designers ', 'architect',
+           'Product Designers ', 'architect', 'Big Data', 'Data Engineering', 'Data Analytics', 'data warehouse', 'data marts',
+           'Kudu', 'HBase', 'HDFS', 'computer vision',
            'backend engineers', 'product designers', 'founder', 'CEO', 'QA engineer', 'big data', 'Front-end Engineer',
            'Full Stack Engineer', 'NLP']
 
@@ -50,14 +53,15 @@ we_list = ['5+ years of experience', '4 years of professional experience', '2-5 
            '2 years of professional experience', '2+ years requirement', "2+ years' experience"
            '5 years professional development experience', '3+ years of WordPress development experience',
            '2-3 years of software developer experience', '5+ years of professional software development experience',
-           '5+ years of industry experience', "3+ years of software engineering experience"]
+           '5+ years of industry experience', "3+ years of software engineering experience",
+           '5+ years previous experience', '3+ years of Drupal experience']
 
-os_list = ['Ubuntu', 'Android', 'Debian', 'iOS', 'Windows', 'Linux', 'CentOS', 'Redhat']
+os_list = ['Ubuntu', 'Android', 'Debian', 'iOS', 'Windows', 'Linux', 'CentOS', 'Redhat', 'RHEL', 'macOS']
 
 ap_list = ['Agile', 'automated tests', 'automated testing', 'Continuous Integration', 'Continuous Delivery', 'CI/CD',
            'Scrum', 'scrum', 'XP', 'automated testing', 'Agile', 'agile', 'git', 'automation', 'TDD', 'object-oriented design',
-           'Microservices', 'waterfall']
+           'Microservices', 'waterfall', 'ETL', 'JIRA']
 
 pf_list = ['AWS', 'Docker', 'Kubernetes', 'Amazon Web Services', 'Google Cloud Platform', 'Google Cloud', 'GCP',
-           'Microsoft Azure', 'Solr', 'Microsoft Cognitive Services', 'OpenSLES', 'Gitlab',
+           'Microsoft Azure', 'Solr', 'Microsoft Cognitive Services', 'OpenSLES', 'Gitlab', 'WebSphere'
            'Heroku', 'Google Analytics', 'OpenStack', 'VMware', 'Github', 'Chrome plugins', 'Unity']
