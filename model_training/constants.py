b26a513b48b48168b97abab4f9b71261e303505d
hejp009 <hejunpeng2012@hotmail.com>
2019-09-27 17:03:14 +0800

    fixed 6th annotations

--- model_training/constants.py
+++ model_training/constants.py
@@ -15,22 +15,22 @@ LABELS = [pl, lf, cp, cs, pt, ds, dv, we, os, ap, pf]
 pl_list = ['JavaScript', 'Javascript', 'javascript', 'JS', 'TypeScript', 'CoffeeScript', 'HTML', 'CSS', 'CSS3', 'PHP', 'ES6', 'Java',
            'java', 'Kotlin', 'kotlin', 'Go', 'go', 'bash', 'C++', 'C', 'c++', 'C#', 'c#', 'Scala', 'ES2015', 'SCSS',
            'Python', 'python', 'Ruby', 'HTML5', 'ES8', 'Golang', 'Rust', "rust", 'Objective C', 'objective-c', 'Obj-C',
-           'Swift', 'swift', 'Perl', 'Erlang', 'Jython', 'Elixir', 'F#', 'ES7', 'GraphQL', 'ES2015+']
+           'Swift', 'swift', 'Perl', 'Erlang', 'Jython', 'Elixir', 'F#', 'ES7', 'GraphQL', 'ES2015+', 'Bash Shell']
 
 lf_list = ['React JS', 'React.js', 'react.js', 'React', 'Redux', 'EcmaScript', 'Immutable.js', 'Babel', 'Webpack', 'NPM',
            'NGINX', 'Laravel', 'Flask', 'Django', 'django', 'scikit-learn', 'Ruby on Rails', 'Rails', 'Node.js', 'NodeJS',
-           'Node', 'Honeybadger', 'Apollo', 'Vue', 'Serverless',
+           'Node', 'Honeybadger', 'Apollo', 'Vue', 'Serverless', 'Numpy', 'CLI', 'React-Native', 'Symfony2',
            'jQuery', 'JQuery', 'Koa', 'Drupal', 'Drush', "Behat", 'Symfony2 ', 'Matplotlib', 'OpenCV', 'Matplotlib,'
            'Numpy', 'SciPy', 'pytest', 'Pytorch', 'Electron', 'Mobx', 'Chef', 'Qt', 'Haml', 'Sass', 'AJAX', 'Bootstrap',
            'Jenkins', 'Backbone', 'backbone.js', 'jUnit', 'Mockito', 'Hamcrest', 'Dagger', 'Terraform', 'Ansible',
            'React Native', 'AngularJS', 'Angular', 'Jest', 'Karma', 'Protractor', 'Cypress', 'Vue.js', 'VUE', 'ASP.NET',
            'Apollo Client', 'WordPress', 'ElasticSearch', 'Elasticsearch', 'Selenium', 'machine learning', 'LAMP', 'MEAN',
-           'Next.js',
+           'Next.js', 'torch', 'SASS',
            'Scrapy', 'Celery', 'RabbitMQ', 'Express', 'RESTify', 'Bootstrap', 'RiotJS', 'LESS', 'Spring', 'Spring boot',
            'Gradle', 'Maven', 'Magento', 'Odoo', 'Oboe', 'Sqoop', 'Hadoop', 'Spark', 'Hive', 'YARN', 'Yarn', 'Gulp',
            'Ember']
 
-cp_list = ['DOM', 'JavaScript object model', 'routing', 'switching']
+cp_list = ['DOM', 'JavaScript object model', 'routing', 'switching', 'MVC', 'MVVM']
 
 cs_list = ['data structure', 'algorithms', 'concurrency', 'object-oriented programming', 'design patterns',
            'Computer Science', "distributed systems", 'OOP', 'Cryptography', 'computability', 'computer architecture',
@@ -42,7 +42,8 @@ pt_list = ['RESTful APIs', 'REST API', 'REST APIs', 'RESTful', 'TCP/IP', 'UDP', 
 
 ds_list = ['JSON', 'PostgreSQL', 'Postgres', 'MongoDB', 'mongodb', 'MySQL', 'mysql', 'ClustrixDB', 'Redis', 'redis', 'SQL',
            'MariaDB', 'InnoDB', 'Kafka', 'Nomad, NoSQL', 'Dynamo', 'Phoenix', 'MS SQL', 'No SQL', 'Firebase', 'HBase',
-           'Memcached', 'relational database', 'Oracle',  'data warehouse', 'data marts', 'Kudu', 'HBase', 'HDFS']
+           'Memcached', 'relational database', 'Oracle',  'data warehouse', 'data marts', 'Kudu', 'HBase', 'HDFS',
+           'MSSQL']
 
 dv_list = ['front-end', 'frontend engineers', 'Web Architect', 'architect', 'Full Stack', 'full stack', 'Android Developer',
            'Android engineer', 'Game Developer', 'artificial intelligence', '3D Rendering', 'architects', 'design',
@@ -51,7 +52,7 @@ dv_list = ['front-end', 'frontend engineers', 'Web Architect', 'architect', 'Ful
            'Product Designers ', 'architect', 'Big Data', 'Data Engineering', 'Data Analytics', 'computer vision',
            'Full-stack', 'backend', 'front end developer', 'QA', 'Project Management',
            'Product Management', 'designers', 'back-end', 'Design', 'frontend', 'Scrum Masters', 'analysts', 'project manager',
-           'full-stack', 'developers', 'designer',
+           'full-stack', 'developers', 'designer', 'Architect',
            'backend engineers', 'product designers', 'founder', 'CEO', 'QA engineer', 'big data', 'Front-end Engineer',
            'Full Stack Engineer', 'NLP']
 
@@ -64,14 +65,17 @@ we_list = ['5+ years of experience', '4 years of professional experience', '2-5 
            '5+ years previous experience', '3+ years of Drupal experience', '5+ years of directly applicable experience',
            '20 years of experience', '5+ years of programming experience', "3-5 years' experience",
            '3+ years in a software engineering or technical architect role',
-           '3+ years of Scrum Master experience']
+           '3+ years of Scrum Master experience', 'Significant macOS experience (+5 years)',
+           '5 years of relevant software development experience', '3-6 years relevant experience',
+           '5+ years of PHP experience']
 
 os_list = ['Ubuntu', 'Android', 'Debian', 'iOS', 'Windows', 'Linux', 'CentOS', 'Redhat', 'RHEL', 'macOS']
 
 ap_list = ['Agile', 'automated tests', 'automated testing', 'Continuous Integration', 'Continuous Delivery', 'CI/CD',
            'Scrum', 'scrum', 'XP', 'automated testing', 'Agile', 'agile', 'git', 'automation', 'TDD', 'object-oriented design',
            'Microservices', 'waterfall', 'ETL', 'JIRA', 'Capistrano', 'automated deployment', 'code-reviews',
-           'pair-programming', 'Jira', 'Lean', 'Git', 'microservices']
+           'pair-programming', 'Jira', 'Lean', 'Git', 'microservices', 'CI', 'refactoring', 'Unit tests',
+           ]
 
 pf_list = ['AWS', 'Docker', 'Kubernetes', 'Amazon Web Services', 'Google Cloud Platform', 'Google Cloud', 'GCP',
            'Microsoft Azure', 'Solr', 'Microsoft Cognitive Services', 'OpenSLES', 'Gitlab', 'WebSphere', 'Xamarin',
--- test/test_annotations.py
+++ test/test_annotations.py
@@ -1,6 +1,6 @@
 import unittest
 from model_training.constants import *
-from model_training.training_07.train_data_07 import TRAIN_DATA
+from model_training.training_06.train_data_06 import TRAIN_DATA
 
 
 class TestAnnotations(unittest.TestCase):
@@ -20,62 +20,72 @@ class TestAnnotations(unittest.TestCase):
     def test_PROGRAMMING_LANGUAGE(self):
         print(set(self.ent_dict['PROGRAMMING_LANGUAGE']))
         for word in set(self.ent_dict['PROGRAMMING_LANGUAGE']):
-            print('testing word: ', word, word in pl_list)
+            if word not in pl_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in pl_list)
 
     def test_LIBRARY_OR_FRAMEWORK(self):
         print(set(self.ent_dict['LIBRARY_OR_FRAMEWORK']))
         for word in set(self.ent_dict['LIBRARY_OR_FRAMEWORK']):
-            print('testing word: ', word, word in lf_list)
+            if word not in lf_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in lf_list)
 
     def test_CONCEPT(self):
         if 'CONCEPT' in self.ent_dict:
             print(set(self.ent_dict['CONCEPT']))
             for word in set(self.ent_dict['CONCEPT']):
-                print('testing word: ', word, word in cp_list)
+                if word not in cp_list:
+                    print('testing word not in list: ', word)
                 self.assertTrue(word in cp_list)
 
     def test_PROTOCOL(self):
         print(set(self.ent_dict['PROTOCOL']))
         for word in set(self.ent_dict['PROTOCOL']):
-            print('testing word: ', word, word in pt_list)
+            if word not in pt_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in pt_list)
 
     def test_DATA_STORAGE(self):
         print(set(self.ent_dict['DATA_STORAGE']))
         for word in set(self.ent_dict['DATA_STORAGE']):
-            print('testing word: ', word, word in ds_list)
+            if word not in ds_list not in list:
+                print('testing word: ', word)
             self.assertTrue(word in ds_list)
 
     def test_DIVISION(self):
         print(set(self.ent_dict['DIVISION']))
         for word in set(self.ent_dict['DIVISION']):
-            print('testing word: ', word, word in dv_list)
+            if word not in dv_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in dv_list)
 
     def test_WORK_EXPERIENCE(self):
         print(set(self.ent_dict['WORK_EXPERIENCE']))
         for word in set(self.ent_dict['WORK_EXPERIENCE']):
-            print('testing word: ', word, word in we_list)
+            if word not in we_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in we_list)
 
     def test_OS(self):
         print(set(self.ent_dict['OS']))
         for word in set(self.ent_dict['OS']):
-            print('testing word: ', word, word in os_list)
+            if word not in os_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in os_list)
 
     def test_APPROACH(self):
         print(set(self.ent_dict['APPROACH']))
         for word in set(self.ent_dict['APPROACH']):
-            print('testing word: ', word, word in ap_list)
+            if word not in ap_list:
+                print('testing word not in list: ', word)
             self.assertTrue(word in ap_list)
 
     def test_PLATFORM(self):
         print(set(self.ent_dict['PLATFORM']))
         for word in set(self.ent_dict['PLATFORM']):
-            print('testing word: ', word, word in pf_list)
+            if word not in pf_list:
+                print('testing word: ', word)
             self.assertTrue(word in pf_list)
 
 
