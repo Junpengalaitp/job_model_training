9ccda006f76d230329cef05e1884dc1346ff41f5
hejp009 <hejunpeng2012@hotmail.com>
2019-09-29 17:09:29 +0800

    add more annotations

--- model_training/constants.py
+++ model_training/constants.py
@@ -13,7 +13,7 @@ pf = 'PLATFORM'
 LABELS = [pl, lf, cp, cs, pt, ds, dv, we, os, ap, pf]
 
 pl_list = ['JavaScript', 'Javascript', 'javascript', 'JS', 'TypeScript', 'CoffeeScript', 'HTML', 'CSS', 'CSS3', 'PHP',
-           'ES6', 'Java', 'ruby', 'Typescript',
+           'ES6', 'Java', 'ruby', 'Typescript', 'XML',
            'java', 'Kotlin', 'kotlin', 'Go', 'go', 'bash', 'C++', 'C', 'c++', 'C#', 'c#', 'Scala', 'ES2015', 'SCSS',
            'Python', 'python', 'Ruby', 'HTML5', 'ES8', 'Golang', 'Rust', "rust", 'Objective C', 'objective-c', 'Obj-C',
            'Swift', 'swift', 'Perl', 'Erlang', 'Jython', 'Elixir', 'F#', 'ES7', 'GraphQL', 'ES2015+', 'Bash Shell']
@@ -23,23 +23,23 @@ lf_list = ['React JS', 'React.js', 'react.js', 'React', 'Redux', 'EcmaScript', '
            'Node', 'Honeybadger', 'Apollo', 'Vue', 'Serverless', 'Numpy', 'CLI', 'React-Native', 'Symfony2', '.NET',
            'jQuery', 'JQuery', 'Koa', 'Drupal', 'Drush', "Behat", 'Symfony2 ', 'Matplotlib', 'OpenCV', 'Matplotlib,'
            'Numpy', 'SciPy', 'pytest', 'Pytorch', 'Electron', 'Mobx', 'Chef', 'Qt', 'Haml', 'Sass', 'AJAX', 'Bootstrap',
-           'Jenkins', 'Backbone', 'backbone.js', 'jUnit', 'Mockito', 'Hamcrest', 'Dagger', 'Terraform', 'Ansible',
+           'Jenkins', 'Backbone', 'backbone.js', 'jUnit', 'Mockito', 'Hamcrest', 'Dagger', 'Terraform', 'Ansible', 'Ajax',
            'React Native', 'AngularJS', 'Angular', 'Jest', 'Karma', 'Protractor', 'Cypress', 'Vue.js', 'VUE', 'ASP.NET',
            'Apollo Client', 'WordPress', 'ElasticSearch', 'Elasticsearch', 'Selenium', 'machine learning', 'LAMP', 'MEAN',
            'Next.js', 'torch', 'SASS', 'redux', 'npm', 'wordpress', 'Jquery', 'big data', 'webpack', 'babel', 'react',
            'Scrapy', 'Celery', 'RabbitMQ', 'Express', 'RESTify', 'Bootstrap', 'RiotJS', 'LESS', 'Spring', 'Spring boot',
            'Gradle', 'Maven', 'Magento', 'Odoo', 'Oboe', 'Sqoop', 'Hadoop', 'Spark', 'Hive', 'YARN', 'Yarn', 'Gulp',
-           'Ember', 'pytorch', 'Flux']
+           'Ember', 'pytorch', 'Flux',]
 
-cp_list = ['DOM', 'JavaScript object model', 'routing', 'switching', 'MVC', 'MVVM']
+cp_list = ['DOM', 'JavaScript object model', 'routing', 'switching', 'MVC', 'MVVM', 'Routing', 'Scheduling']
 
 cs_list = ['data structure', 'algorithms', 'concurrency', 'object-oriented programming', 'design patterns',
            'Computer Science', "distributed systems", 'OOP', 'Cryptography', 'computability', 'computer architecture',
-           'concurrency', 'parallelism']
+           'concurrency', 'parallelism', 'Mathematics', 'Statistics']
 
 pt_list = ['RESTful APIs', 'REST API', 'REST APIs', 'RESTful', 'TCP/IP', 'UDP', 'browser APIs', 'HTTP', 'WebSockets',
            'WebRTC', 'API', 'APIs', 'firewalls', 'Serverless', 'CORS', 'SSL', 'Paxos', 'WASAPI', 'CoreAudio', 'wasm',
-           'SFTP', 'SSH', 'TLS', 'FTP']
+           'SFTP', 'SSH', 'TLS', 'FTP', 'REST']
 
 ds_list = ['JSON', 'PostgreSQL', 'Postgres', 'MongoDB', 'mongodb', 'MySQL', 'mysql', 'ClustrixDB', 'Redis', 'redis', 'SQL',
            'MariaDB', 'InnoDB', 'Kafka', 'Nomad, NoSQL', 'Dynamo', 'Phoenix', 'MS SQL', 'No SQL', 'Firebase', 'HBase',
@@ -52,12 +52,13 @@ dv_list = ['front-end', 'frontend engineers', 'Web Architect', 'architect', 'Ful
            'Full Stack Developer', 'QA testers', 'DevOps', 'frontend stack', 'frontend UIs', 'product designers',
            'Product Designers ', 'architect', 'Big Data', 'Data Engineering', 'Data Analytics', 'computer vision',
            'Full-stack', 'backend', 'front end developer', 'QA', 'Project Management', '3D animation', 'full stack software developer',
-           'UX design', 'Backend', 'UI/UX designer', 'product managers', 'engineers', 'Back-end development',
+           'UX design', 'Backend', 'UI/UX designer', 'product managers', 'engineers', 'Back-end development', 'R&D',
            'Product Management', 'designers', 'back-end', 'Design', 'frontend', 'Scrum Masters', 'analysts', 'project manager',
            'full-stack', 'developers', 'designer', 'Architect', 'audio', 'front-end development', 'QA engineers',
            'backend engineers', 'product designers', 'founder', 'CEO', 'QA engineer', 'big data', 'Front-end Engineer',
            'Full Stack Engineer', 'NLP', 'Full Stack Engineers', 'full stack engineer', 'Frontend Developers',
-           'back-end developers', 'Web', 'senior software engineer', 'Sr. Software Engineer', 'back end', 'front end',]
+           'back-end developers', 'Web', 'senior software engineer', 'Sr. Software Engineer', 'back end', 'front end',
+           'Data Developer/Engineer']
 
 we_list = ['5+ years of experience', '4 years of professional experience', '2-5 years of experience', '2+ years focused on',
            '2+ years of client-side Javascript experience', '5+ years experience ', '1+ years experience',
@@ -76,15 +77,16 @@ we_list = ['5+ years of experience', '4 years of professional experience', '2-5 
            '5+ years of applicable experience', '3+ years of experience', '2 years of Ruby on Rails Experience',
            '3+ years of professional experience', '10+ years of professional engineering experience',
            '3 years of Ruby on Rails Experience', '5 years of it being Ruby on Rails',
+           '3 years of relevant data development and technology experience', '8+ years programming experience',
            ]
 
-os_list = ['Ubuntu', 'Android', 'Debian', 'iOS', 'Windows', 'Linux', 'CentOS', 'Redhat', 'RHEL', 'macOS']
+os_list = ['Ubuntu', 'Android', 'Debian', 'iOS', 'Windows', 'Linux', 'CentOS', 'Redhat', 'RHEL', 'macOS', 'Unix']
 
 ap_list = ['Agile', 'automated tests', 'automated testing', 'Continuous Integration', 'Continuous Delivery', 'CI/CD',
            'Scrum', 'scrum', 'XP', 'automated testing', 'Agile', 'agile', 'git', 'automation', 'TDD', 'object-oriented design',
            'Microservices', 'waterfall', 'ETL', 'JIRA', 'Capistrano', 'automated deployment', 'code-reviews', 'SCRUM',
-           'pair-programming', 'Jira', 'Lean', 'Git', 'microservices', 'CI', 'refactoring', 'Unit tests',
-           'microservice', 'continuous integration', 'Git-Flow', 'unit testing', 'continuous delivery',
+           'pair-programming', 'Jira', 'Lean', 'Git', 'microservices', 'CI', 'refactoring', 'Unit tests', 'gitflow',
+           'microservice', 'continuous integration', 'Git-Flow', 'unit testing', 'continuous delivery', 'responsive design',
            ]
 
 pf_list = ['AWS', 'Docker', 'Kubernetes', 'Amazon Web Services', 'Google Cloud Platform', 'Google Cloud', 'GCP',
--- test/test_annotations.py
+++ test/test_annotations.py
@@ -1,6 +1,6 @@
 import unittest
 from model_training.constants import *
-from model_training.training_01.train_data_01 import TRAIN_DATA
+from model_training.training_08.train_data_08 import TRAIN_DATA
 
 
 class TestAnnotations(unittest.TestCase):
