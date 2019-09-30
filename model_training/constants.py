afcb228682b758dc3740ad124a98061b05f3f24f
hejp009 <hejunpeng2012@hotmail.com>
2019-09-30 11:29:32 +0800

    8th annotation complete

--- model_training/constants.py
+++ model_training/constants.py
@@ -21,7 +21,7 @@ pl_list = ['JavaScript', 'Javascript', 'javascript', 'JS', 'TypeScript', 'Coffee
 lf_list = ['React JS', 'React.js', 'react.js', 'React', 'Redux', 'EcmaScript', 'Immutable.js', 'Babel', 'Webpack', 'NPM',
            'NGINX', 'Laravel', 'Flask', 'Django', 'django', 'scikit-learn', 'Ruby on Rails', 'Rails', 'Node.js', 'NodeJS',
            'Node', 'Honeybadger', 'Apollo', 'Vue', 'Serverless', 'Numpy', 'CLI', 'React-Native', 'Symfony2', '.NET',
-           'jQuery', 'JQuery', 'Koa', 'Drupal', 'Drush', "Behat", 'Symfony2 ', 'Matplotlib', 'OpenCV', 'Matplotlib,'
+           'jQuery', 'JQuery', 'Koa', 'Drupal', 'Drush', "Behat", 'OpenCV', 'Matplotlib', 'chai', 'jasmine', 'sinon',
            'Numpy', 'SciPy', 'pytest', 'Pytorch', 'Electron', 'Mobx', 'Chef', 'Qt', 'Haml', 'Sass', 'AJAX', 'Bootstrap',
            'Jenkins', 'Backbone', 'backbone.js', 'jUnit', 'Mockito', 'Hamcrest', 'Dagger', 'Terraform', 'Ansible', 'Ajax',
            'React Native', 'AngularJS', 'Angular', 'Jest', 'Karma', 'Protractor', 'Cypress', 'Vue.js', 'VUE', 'ASP.NET',
@@ -29,7 +29,7 @@ lf_list = ['React JS', 'React.js', 'react.js', 'React', 'Redux', 'EcmaScript', '
            'Next.js', 'torch', 'SASS', 'redux', 'npm', 'wordpress', 'Jquery', 'big data', 'webpack', 'babel', 'react',
            'Scrapy', 'Celery', 'RabbitMQ', 'Express', 'RESTify', 'Bootstrap', 'RiotJS', 'LESS', 'Spring', 'Spring boot',
            'Gradle', 'Maven', 'Magento', 'Odoo', 'Oboe', 'Sqoop', 'Hadoop', 'Spark', 'Hive', 'YARN', 'Yarn', 'Gulp',
-           'Ember', 'pytorch', 'Flux',]
+           'Ember', 'pytorch', 'Flux', 'Backbone.js', 'teaspoon', 'Axios']
 
 cp_list = ['DOM', 'JavaScript object model', 'routing', 'switching', 'MVC', 'MVVM', 'Routing', 'Scheduling']
 
@@ -44,10 +44,10 @@ pt_list = ['RESTful APIs', 'REST API', 'REST APIs', 'RESTful', 'TCP/IP', 'UDP', 
 ds_list = ['JSON', 'PostgreSQL', 'Postgres', 'MongoDB', 'mongodb', 'MySQL', 'mysql', 'ClustrixDB', 'Redis', 'redis', 'SQL',
            'MariaDB', 'InnoDB', 'Kafka', 'Nomad, NoSQL', 'Dynamo', 'Phoenix', 'MS SQL', 'No SQL', 'Firebase', 'HBase',
            'Memcached', 'relational database', 'Oracle',  'data warehouse', 'data marts', 'Kudu', 'HBase', 'HDFS',
-           'MSSQL', 'NOSQL', 'NoSQL']
+           'MSSQL', 'NOSQL', 'NoSQL', 'relational database systems']
 
 dv_list = ['front-end', 'frontend engineers', 'Web Architect', 'architect', 'Full Stack', 'full stack', 'Android Developer',
-           'Android engineer', 'Game Developer', 'artificial intelligence', '3D Rendering', 'architects', 'design',
+           'Android engineer', 'Game Developer', 'artificial intelligence', '3D Rendering', 'architects', 'design', 'PM',
            'UI designs', 'UI', 'UX', 'AI', 'Rails Engineer', 'Product Managers', 'Designers', 'CTO', 'Web Developer',
            'Full Stack Developer', 'QA testers', 'DevOps', 'frontend stack', 'frontend UIs', 'product designers',
            'Product Designers ', 'architect', 'Big Data', 'Data Engineering', 'Data Analytics', 'computer vision',
@@ -58,7 +58,9 @@ dv_list = ['front-end', 'frontend engineers', 'Web Architect', 'architect', 'Ful
            'backend engineers', 'product designers', 'founder', 'CEO', 'QA engineer', 'big data', 'Front-end Engineer',
            'Full Stack Engineer', 'NLP', 'Full Stack Engineers', 'full stack engineer', 'Frontend Developers',
            'back-end developers', 'Web', 'senior software engineer', 'Sr. Software Engineer', 'back end', 'front end',
-           'Data Developer/Engineer']
+           'Data Developer/Engineer', 'Senior Software Engineer', 'web developer', 'Senior Engineer', 'Senior Front End Engineer',
+           'FRONT END WEB DEVELOPER', 'business analysts', 'Back End Developers', 'Back End Development',
+           ]
 
 we_list = ['5+ years of experience', '4 years of professional experience', '2-5 years of experience', '2+ years focused on',
            '2+ years of client-side Javascript experience', '5+ years experience ', '1+ years experience',
@@ -76,8 +78,10 @@ we_list = ['5+ years of experience', '4 years of professional experience', '2-5 
            '7+ years in the software development industryExperience ', '5 years professional development experience',
            '5+ years of applicable experience', '3+ years of experience', '2 years of Ruby on Rails Experience',
            '3+ years of professional experience', '10+ years of professional engineering experience',
-           '3 years of Ruby on Rails Experience', '5 years of it being Ruby on Rails',
+           '3 years of Ruby on Rails Experience', '5 years of it being Ruby on Rails', '4+ years programming experience',
            '3 years of relevant data development and technology experience', '8+ years programming experience',
+           '3+ years of Ruby on Rails experience', '5+ years of professional Front End Development experience',
+           "7+ years of Application Development with experience defining development guidelines"
            ]
 
 os_list = ['Ubuntu', 'Android', 'Debian', 'iOS', 'Windows', 'Linux', 'CentOS', 'Redhat', 'RHEL', 'macOS', 'Unix']
@@ -87,10 +91,11 @@ ap_list = ['Agile', 'automated tests', 'automated testing', 'Continuous Integrat
            'Microservices', 'waterfall', 'ETL', 'JIRA', 'Capistrano', 'automated deployment', 'code-reviews', 'SCRUM',
            'pair-programming', 'Jira', 'Lean', 'Git', 'microservices', 'CI', 'refactoring', 'Unit tests', 'gitflow',
            'microservice', 'continuous integration', 'Git-Flow', 'unit testing', 'continuous delivery', 'responsive design',
+           'test-driven development',
            ]
 
 pf_list = ['AWS', 'Docker', 'Kubernetes', 'Amazon Web Services', 'Google Cloud Platform', 'Google Cloud', 'GCP',
            'Microsoft Azure', 'Solr', 'Microsoft Cognitive Services', 'OpenSLES', 'Gitlab', 'WebSphere', 'Xamarin',
            'Heroku', 'Google Analytics', 'OpenStack', 'VMware', 'Github', 'Chrome plugins', 'Unity', 'Terraform',
            'Slack', 'Trello', 'Ghost Inspector', 'CircleCI', 'Google Docs', 'Google Hangouts', 'Azure', 'GitHub',
-           'ERP', 'Kafka',]
+           'ERP', 'Kafka', 'GCE', 'Confluence', 'Zoom',]
