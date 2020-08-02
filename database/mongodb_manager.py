5f9f3bfa112d92e7b26008c2475d394e221b89bc
Junpeng He <hejunpeng2012@hotmail.com>
2020-08-02 20:09:56 +0800

    clean

--- database/mongodb_manager.py
+++ database/mongodb_manager.py
@@ -1,18 +1,12 @@
-import logging
-
 from pymongo import MongoClient
 
-from logger.logger import setup_logging
-from config import read_config
-
-setup_logging()
-logger = logging.getLogger("dbLogger")
+from config.settings import MONGO_IP, MONGO_PORT
 
 
 class MongoManager:
     def __init__(self):
-        mongo_ip = read_config.get_config('mongodb', 'ip')
-        mongo_port = int(read_config.get_config('mongodb', 'port'))
+        mongo_ip = MONGO_IP
+        mongo_port = MONGO_PORT
         # username = read_config.get_config('mongodb', 'username')
         # password = read_config.get_config('mongodb', 'password')
         # self.client = MongoClient(f'mongodb://{username}:{password}@{mongo_ip}', mongo_port)
--- database/sqlalchemy_manager.py
+++ database/sqlalchemy_manager.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 
-from config.read_config import get_config
+from config.settings import SQL_IP, DB_PORT, DB_NAME, USERNAME, PASSWORD
 
-SERVER_IP = get_config('SQL_SERVER', 'ip')
-PORT = get_config('SQL_SERVER', 'port')
-DB_NAME = get_config('SQL_SERVER', 'dbname')
-username = get_config('SQL_SERVER', 'user')
-password = get_config('SQL_SERVER', 'password')
+SERVER_IP = SQL_IP
+PORT = DB_PORT
+db_name = DB_NAME
+username = USERNAME
+password = PASSWORD
 
 conn = create_engine(f'postgresql://{username}:{password}@{SERVER_IP}:{PORT}/{DB_NAME}', pool_recycle=3600)
 
