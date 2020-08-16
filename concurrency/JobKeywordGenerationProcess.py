5dc78a86b28040315f25c4440b585763a38f0b0e
Junpeng He <hejunpeng2012@hotmail.com>
2020-08-16 21:14:15 +0800

    feat(keyword): use multiprocessing

---
+++ concurrency/JobKeywordGenerationProcess.py
@@ -0,0 +1,62 @@
+import traceback
+from datetime import datetime
+from multiprocessing import Process
+
+import pandas as pd
+from loguru import logger
+
+from concurrency.CountDownLatch import CountDownLatch
+from database.sql_operation.standard_word import insert_model_keywords, select_standard_word, select_standard_category
+
+
+class JobKeywordGenerationProcess(Process):
+    def __init__(self, df_row, nlp):
+        super().__init__()
+        self.df_row = df_row
+        self.nlp = nlp
+
+    def run(self, count_down_latch: CountDownLatch) -> None:
+        super().run()
+        count_down_latch.count_down()
+        self.process_job_df()
+        logger.info(f" remaining works: {count_down_latch.count}")
+
+
+    def process_job_df(self):
+        try:
+            job_keyword_df = self.generate_job_key_words()
+            insert_model_keywords(job_keyword_df)
+        except:
+            return
+
+    def generate_job_key_words(self):
+        job_desc = self.df_row.job_desc
+        doc = self.nlp(job_desc)
+        result_df = pd.DataFrame(
+            columns=['keyword_name', 'standard_word', 'keyword_type', 'count', "job_title", "job_id", "source",
+                     "created_time"])
+        result_df['keyword_name'] = [w.text for w in doc.ents if not w.text.isspace()]
+        result_df['keyword_type'] = [w.label_ for w in doc.ents if not w.text.isspace()]
+        result_df['count'] = [result_df['keyword_name'].values.tolist().count(e) for e in
+                              result_df['keyword_name'].values.tolist()]
+        result_df.drop_duplicates('keyword_name', inplace=True)
+        result_df.reset_index(drop=True, inplace=True)
+
+        for row in result_df.itertuples():
+            try:
+                standard_word = select_standard_word(row.keyword_name)
+                result_df.at[row.Index, 'standard_word'] = standard_word
+                standard_category = select_standard_category(standard_word)
+                if standard_category != "" and standard_category != row.keyword_type:
+                    result_df.at[row.Index, 'keyword_type'] = standard_category
+                    logger.debug(f"standardized category of word: {row.keyword_name} from {row.keyword_type} to {standard_category}")
+            except:
+                traceback.print_exc()
+                logger.error(f"error word: {row.keyword_name}")
+                continue
+
+        result_df['job_title'] = self.df_row.title
+        result_df['job_id'] = self.df_row.job_id
+        result_df['source'] = 'dice'
+        result_df['created_time'] = datetime.now()
+        return result_df
--- scripts/job_keyword_generation.py
+++ scripts/job_keyword_generation.py
@@ -1,12 +1,13 @@
 import logging
 import os
 import sys
-from concurrent.futures.thread import ThreadPoolExecutor
+from concurrent.futures.process import ProcessPoolExecutor
 
 import spacy
 from loguru import logger
 
 from concurrency.CountDownLatch import CountDownLatch
+from concurrency.JobKeywordGenerationProcess import JobKeywordGenerationProcess
 from concurrency.JobKeywordGenerationThread import JobKeywordGenerationThread
 from database.sql_operation.standard_word import select_all_dice_jobs
 from util.timer import timeit
@@ -18,14 +19,15 @@ sys.path.append(model_path)
 
 nlp = spacy.load('job_model_sm')
 
+
 @timeit
 def run():
     jobs_df = select_all_dice_jobs()
     jobs_amount = len(jobs_df)
     logging.info(f"retrieved {jobs_amount} jobs")
-    threads = [JobKeywordGenerationThread(job_df, nlp) for job_df in jobs_df.itertuples()]
+    threads = [JobKeywordGenerationProcess(job_df, nlp) for job_df in jobs_df.itertuples()]
     count_down_latch = CountDownLatch(len(threads))
-    with ThreadPoolExecutor(max_workers=os.cpu_count() * 4) as executor:
+    with ProcessPoolExecutor(max_workers=os.cpu_count() * 4) as executor:
         for thread in threads:
             try:
                 executor.submit(thread.run(count_down_latch))
